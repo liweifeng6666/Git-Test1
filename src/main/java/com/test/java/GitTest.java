0f500446408f0c61b5fcc4c3148194fbe1dbff09
lwf <lwf@qq.com>
2021-09-14 13:09:42 +0800

    first commit

---
+++ src/main/java/com/test/java/GitTest.java
@@ -0,0 +1,14 @@
+package com.test.java;
+
+/**
+ * @ClassName GitTest
+ * @Description:
+ * @Author LiWeiFeng
+ * @LastChangeDate 2021/9/14 13:03
+ * @Version v1.8
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
---
+++ src/main/java/com/test/java/HelloGit.java
@@ -0,0 +1,11 @@
+package com.test.java;
+
+/**
+ * @ClassName HelloGit
+ * @Description:
+ * @Author LiWeiFeng
+ * @LastChangeDate 2021/9/14 13:07
+ * @Version v1.8
+ */
+public class HelloGit {
+}
