5a08e1ca87845292625c89b69e9b4ef7c4a4765a
李维锋 <75901410+liweifeng6666@users.noreply.github.com>
2021-09-14 14:41:00 +0800

    pull commit

--- src/main/java/com/test/java/GitTest.java
+++ src/main/java/com/test/java/GitTest.java
@@ -9,14 +9,10 @@ package com.test.java;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git1");
-        System.out.println("hello git2");
-        System.out.println("hello git3");
-        System.out.println("hello git4");
-        System.out.println("master-test");
-        System.out.println("hello git4");
+      
+       System.out.println("hello git4");
         System.out.println("hot-fix test");
         System.out.println("push test");
+         System.out.println("pull test");
     }
 }
